b97b7015a72225d235b9d446005924bbb98906d2
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2023-01-20 19:07:38 +0530

    Add interaction

--- js/interactions.js
+++ js/interactions.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", function() {
+  var form = document.getElementById("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    var rating = Object.fromEntries(formData).component__rating;
+    if (rating) {
+      document.getElementById("rating").setAttribute("hidden", "hidden");
+      document.getElementById("response").removeAttribute("hidden");
+      document.getElementById("feedback").textContent = rating;
+    }
+  });
+});
